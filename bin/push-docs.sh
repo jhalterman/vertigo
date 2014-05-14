0985e64d92f71652c4094dc80c3c065f0cb12bbf
Jonathan Halterman <jhalterman@gmail.com>
2014-05-14 15:31:05 -0700

    initial commit

--- bin/push-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf docs
-git clone git@github.com:kuujo/vertigo-docs.git docs -b gh-pages
-mvn -Pjavadoc javadoc:javadoc
-cd docs
-git add -A
-git commit -m "Updated documentation."
-git push origin gh-pages
